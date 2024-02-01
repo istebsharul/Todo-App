040b14b6f4569f07717baeb4be79ff0c4d8498fa
istebsharul <istebsharulbari919@gmail.com>
2024-02-01 17:15:27 +0530

    Frontend Completed

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import React from "react";
+import { useTodo } from "../contexts/TodoContext";
 
 function TodoForm() {
+  const [todo, setTodo] = useState("");
+
+  const { addTodo } = useTodo();
+
+  const add = (e) => {
+    e.preventDefault();
+
+    if (!todo) return;
+
+    // addTodo({id: Date.now(), todo: todo, completed: false})
+    // since Date.now is Already there in function & (todo:todo is Same as todo) in New Syntax Format of Add todo
+    addTodo({ todo: todo, completed: false });
+    setTodo("");
+  };
+
   return (
-    <>
-      <form className="flex">
-        <input
-          type="text"
-          placeholder="Write Todo..."
-          className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-        />
-        <button
-          type="submit"
-          className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
-        >
-          Add
-        </button>
-      </form>
-    </>
+    <form onSubmit={add} className="flex">
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
   );
 }
 
